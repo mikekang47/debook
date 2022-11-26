2b20039e97a907e851bd4db3e640516ae39b2c7f
mikekang47 <gyeong0308@gmail.com>
2022-11-27 01:01:32 +0900

    Init project

---
+++ src/main/java/com/sihoo/me/debookprivate/DebookPrivateApplication.java
@@ -0,0 +1,13 @@
+package com.sihoo.me.debookprivate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebookPrivateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebookPrivateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihoo/me/debookprivate/DebookPrivateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihoo.me.debookprivate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebookPrivateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
