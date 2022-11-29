e7bf174b4b05a6527c71b61fb87b66d582dbebee
mikekang47 <gyeong0308@gmail.com>
2022-11-29 17:06:03 +0900

    Feat: add jpa config for querydsl

---
+++ src/main/java/com/sihoo/me/debook/config/JpaConfig.java
@@ -0,0 +1,21 @@
+package com.sihoo.me.debook.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@EnableJpaRepositories(basePackages = "com.sihoo.me")
+@Configuration
+public class JpaConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
