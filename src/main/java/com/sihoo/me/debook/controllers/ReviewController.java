c93867fdfb2aca74ddda2c7a85588a6e9ce839ee
hoo <sihookang47@gmail.com>
2022-12-01 01:48:37 +0900

    Feat: add delete method and tests

--- src/main/java/com/sihoo/me/debook/controllers/ReviewController.java
+++ src/main/java/com/sihoo/me/debook/controllers/ReviewController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
 import javax.validation.Valid;
 import java.util.List;
 
