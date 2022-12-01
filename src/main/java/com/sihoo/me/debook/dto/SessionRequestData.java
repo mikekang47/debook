231c5171de1310f6b728b66161c4932b18dda2d2
hoo <sihookang47@gmail.com>
2022-12-01 16:23:35 +0900

    Feat: add NoArgsConstructor for requestBody

--- src/main/java/com/sihoo/me/debook/dto/SessionRequestData.java
+++ src/main/java/com/sihoo/me/debook/dto/SessionRequestData.java
@@ -3,6 +3,7 @@ package com.sihoo.me.debook.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -12,6 +13,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @Getter
 @Builder
+@NoArgsConstructor
 public class SessionRequestData {
     @Email(message = "[ERROR] Email must wel-formed email address")
     @NotEmpty(message = "[ERROR] Email can not be empty or null")
--- src/main/java/com/sihoo/me/debook/dto/SessionResponseData.java
+++ src/main/java/com/sihoo/me/debook/dto/SessionResponseData.java
@@ -2,8 +2,10 @@ package com.sihoo.me.debook.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SessionResponseData {
     private String accessToken;
